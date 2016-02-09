109964f11051a408426bb5b7dea1a6370a2a971c
vemacs <d@nkmem.es>
2016-02-09 12:20:20 -0700

    Decrease priorities

--- src/main/java/es/nkmem/da/spaghetti/state/StateManager.java
+++ src/main/java/es/nkmem/da/spaghetti/state/StateManager.java
@@ -5,15 +5,14 @@ import es.nkmem.da.spaghetti.data.WrappedLocation;
 import es.nkmem.da.spaghetti.handlers.PlayerResetHandler;
 import es.nkmem.da.spaghetti.handlers.WorldHandler;
 import es.nkmem.da.spaghetti.registries.StateListenerRegistry;
-import es.nkmem.da.spaghetti.registries.StateScheduler;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -27,12 +26,11 @@ public class StateManager {
     }
 
     /**
-     *
-     * @param state The game state to move to
+     * @param state      The game state to move to
      * @param transition Details of the transition
-     * @param data Data that is passed to the next state (consider this something similar to an Android bundle)
+     * @param data       Data that is passed to the next state (consider this something similar to an Android bundle)
      */
-    public void nextState(AbstractGameState state, Transition transition, MemoryConfiguration data) {
+    public void nextState(@NonNull AbstractGameState state, Transition transition, MemoryConfiguration data) {
         // cleanup
         currentState.getCommandRegistry().shutdown();
         currentState.getListenerRegistry().shutdown();
@@ -40,7 +38,7 @@ public class StateManager {
         currentState.cleanup();
 
         spaghetti.getLogger().info("Switching from " + currentState.getClass().getSimpleName() + " to " +
-            state.getClass().getSimpleName() + ", with" + transition.toString());
+                state.getClass().getSimpleName() + ", with" + transition.toString());
         currentState = null; // Release references
 
         // handle transition
@@ -69,21 +67,16 @@ public class StateManager {
         }
 
         // initialize new state
-        if (state == null) {
-            spaghetti.getLogger().warning("A null game state was passed. Was this expected behavior?");
-            state = new NullGameState(spaghetti);
-        } else {
-            StateListenerRegistry listenerRegistry = state.getListenerRegistry();
-            if (transition.getTeleportTo() != null) {
-                listenerRegistry.registerListener(
-                        new TeleportListener(transition.getTeleportTo().getBukkitLocation().clone())
-                );
-            }
-            if (transition.getResetPlayers()) {
-                listenerRegistry.registerListener(
-                        new ResetListener()
-                );
-            }
+        StateListenerRegistry listenerRegistry = state.getListenerRegistry();
+        if (transition.getTeleportTo() != null) {
+            listenerRegistry.registerListener(
+                    new TeleportListener(transition.getTeleportTo().getBukkitLocation().clone())
+            );
+        }
+        if (transition.getResetPlayers()) {
+            listenerRegistry.registerListener(
+                    new ResetListener()
+            );
         }
         currentState = state;
         state.initialize(data);
@@ -102,14 +95,14 @@ public class StateManager {
         @NonNull
         private final Location teleportDest;
 
-        @EventHandler
+        @EventHandler(priority = EventPriority.LOWEST)
         public void onPlayerJoin(PlayerJoinEvent event) {
             event.getPlayer().teleport(teleportDest);
         }
     }
 
     private class ResetListener implements Listener {
-        @EventHandler
+        @EventHandler(priority = EventPriority.LOWEST)
         public void onPlayerJoin(PlayerJoinEvent event) {
             PlayerResetHandler.resetPlayer(event.getPlayer());
         }
